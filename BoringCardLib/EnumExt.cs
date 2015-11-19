e0ecb76e30d896b0f2803e9587748e382cb3e710
LazyBui <Lazy.Bui@Gmail.com>
2015-11-19 13:51:56 -0600

    Fixing up underlying type of enum values (array could be mutated, if a bit awkwardly)

--- BoringCardLib/EnumExt.cs
+++ BoringCardLib/EnumExt.cs
@@ -5,7 +5,12 @@ using System.Linq;
 
 namespace BoringCardLib {
 	internal static class EnumExt<TValue> {
-		private static Lazy<IEnumerable<TValue>> sCache = new Lazy<IEnumerable<TValue>>(() => Enum.GetValues(typeof(TValue)).Cast<TValue>());
+		private static Lazy<IEnumerable<TValue>> sCache =
+			new Lazy<IEnumerable<TValue>>(() =>
+				Enum.GetValues(typeof(TValue)).
+					Cast<TValue>().
+					ToList().
+					AsReadOnly());
 
 		public static IEnumerable<TValue> GetValues() {
 			return sCache.Value;
@@ -15,7 +20,9 @@ namespace BoringCardLib {
 	internal static class EnumExt {
 		public static void ThrowIfInvalid(this Enum pValue, string pName = null) {
 			if (pValue == null) return;
-			if (!Enum.IsDefined(pValue.GetType(), pValue)) throw new ArgumentException("Must have a valid value", pName);
+			if (!Enum.IsDefined(pValue.GetType(), pValue)) {
+				throw new ArgumentException("Must have a valid value", pName);
+			}
 		}
 	}
 }
