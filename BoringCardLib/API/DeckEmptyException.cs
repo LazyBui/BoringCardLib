e144afbd09a4e4830e0d65670ebc8484bfe4ec17
LazyBui <Lazy.Bui@Gmail.com>
2017-10-31 23:42:23 -0500

    Preventing outside parties from creating a DeckEmptyException since it should only need to be caught by callers, not generated

--- BoringCardLib/API/DeckEmptyException.cs
+++ BoringCardLib/API/DeckEmptyException.cs
@@ -5,10 +5,7 @@ namespace BoringCardLib {
 	/// Represents an exception generated due to an empty deck.
 	/// </summary>
 	public sealed class DeckEmptyException : InvalidOperationException {
-		/// <summary>
-		/// Initializes a new instance of the <see cref="DeckEmptyException" /> class. 
-		/// </summary>
-		public DeckEmptyException() : base("The deck is empty and this operation is unsupported") {
+		internal DeckEmptyException() : base("The deck is empty and this operation is unsupported") {
 
 		}
 	}
